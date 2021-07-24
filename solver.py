6813422394369723d4b4f415ea70b575f1ab9566
kac3pro <kac3pro@gmail.com>
2021-07-24 19:39:20 +0200

    finished

--- solver.py
+++ solver.py
@@ -1,4 +1,18 @@
 import rubik
+from collections import deque
+
+
+# only for testing
+def index(dq, x):
+    for i in range(len(dq)):
+        if dq[i] == x:
+            return i
+    return -1
+
+
+def print_output(path):
+    for i in path:
+        print(rubik.quarter_twists_names[i])
 
 
 def neighbours(position):
@@ -7,8 +21,16 @@ def neighbours(position):
     """
     res = []
     for operation in rubik.quarter_twists:
-        res.append((position.perm_apply(operation), operation))
+        res.append((rubik.perm_apply(position, operation), operation))
     return res
+
+
+def validity_check(key, predecessor):
+    for i in range(1, 8):
+        key = predecessor[key][0]
+        if predecessor[key][0] == key:
+            return True
+    return False
 
 
 def shortest_path(start, end):
@@ -19,6 +41,8 @@ def shortest_path(start, end):
     You can use the rubik.quarter_twists move set.
     Each move can be applied using rubik.perm_apply
     """
+    if start == end:
+        return []
     # helper structures
     # stores elements that have already been popped from at least one queue
     popped = {}
@@ -29,18 +53,23 @@ def shortest_path(start, end):
     predecessor_f = {}
     predecessor_b = {}
     # a fifo queue used by BFS
-    queue_f = []
-    queue_b = []
+    queue_f = deque()
+    queue_b = deque()
 
     queue_f.append(start)
     queue_b.append(end)
     distance_f[start] = 0
     distance_b[end] = 0
+    predecessor_f[start] = (start, None)
+    predecessor_b[end] = (end, None)
 
     terminate = False
     w = None
+    counter = 1
     while not terminate:
-        u = queue_f.pop()
+        u = queue_f.popleft()
+        if not validity_check(u, predecessor_f):
+            return None
         if u not in popped:
             popped[u] = 1
         else:
@@ -50,13 +79,14 @@ def shortest_path(start, end):
             if v[0] not in predecessor_f:
                 distance_f[v[0]] = distance_f[u] + 1
                 predecessor_f[v[0]] = (u, v[1])
-                print v[0]
                 queue_f.append(v[0])
 
-        u = queue_b.pop()
+        u = queue_b.popleft()
+        if not validity_check(u, predecessor_b):
+            return None
         if u not in popped:
             popped[u] = 1
-        else:
+        elif not terminate:
             terminate = True
             w = u
         for v in neighbours(u):
@@ -66,19 +96,15 @@ def shortest_path(start, end):
                 queue_b.append(v[0])
 
     path_f = []
-    path_f.append(w)
     v = w
-    while predecessor_f[v] is not None:
+    while v != start:
         path_f.append(predecessor_f[v][1])
         v = predecessor_f[v][0]
-    path_f.append(v)
     path_f.reverse()
     path_b = []
-    while predecessor_b[w] is not None:
-        path_b.append(predecessor_b[w][1])
+    while w != end:
+        path_b.append(rubik.perm_inverse(predecessor_b[w][1]))
         w = predecessor_b[w][0]
-    path_f.append(w)
     path_f.extend(path_b)
+    path_f.reverse()
     return path_f
-
-
